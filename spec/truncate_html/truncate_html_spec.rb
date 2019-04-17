45517c7744fb241edfdd78e5123e4c3bad70ff73
Xenofon Deligiannis <xdeligiannis@skroutz.gr>
2019-04-17 18:27:08 +0300

    Add expect to specs and use latest ruby hash syntax

--- spec/truncate_html/truncate_html_spec.rb
+++ spec/truncate_html/truncate_html_spec.rb
@@ -1,15 +1,19 @@
 require File.join(File.dirname(__FILE__), '..', 'spec_helper')
 
 describe TruncateHtml do
-  it "includes itself in ActionController::Base" do
-    ActionController::Base.should_receive(:helper).with(TruncateHtmlHelper)
-    load File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'lib', 'truncate_html.rb'))
+  it 'includes itself in ActionController::Base' do
+    expect(ActionController::Base).to receive(:helper).with(TruncateHtmlHelper)
+    load File.expand_path(
+      File.join(File.dirname(__FILE__), '..', '..', 'lib', 'truncate_html.rb')
+    )
   end
 
-  it "does not error if ActionController is undefined" do
-    hide_const("ActionController")
+  it 'does not error if ActionController is undefined' do
+    hide_const('ActionController')
     expect {
-      load File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'lib', 'truncate_html.rb'))
+      load File.expand_path(
+        File.join(File.dirname(__FILE__), '..', '..', 'lib', 'truncate_html.rb')
+      )
     }.not_to raise_error
   end
 end
